9199bd002dd2628e1c434b26cedda80ce22967c2
i.shatunova <ivashatunova@gmail.com>
2023-08-26 17:21:34 +0300

    Add successful login test

---
+++ 7.3/playwright/tests/login.test.js
@@ -0,0 +1,24 @@
+const { test, expect } = require("@playwright/test");
+const { user, password } = require("../user");
+
+test("login success", async ({ page }) => {
+  // Go to https://netology.ru/?modal=sign_in
+  await page.goto("https://netology.ru/?modal=sign_in");
+
+  // Добавляем email
+  await page.fill('input[name="email"][placeholder="Email"]', user);
+
+  // Добавляем пароль
+  await page.fill('input[name="password"]', password);
+
+  // Click text=Войти
+  await page.click('button[type="submit"]');
+  await expect(page).toHaveURL("https://netology.ru/profile");
+
+  // Проверяем, что мы авторизовались успешно
+  // Замените 'selector_of_success_message' на текст, который вы ожидаете увидеть после успешной авторизации
+  const successMessage = await page.waitForSelector(
+    "h2.src-components-pages-Profile-Programs--title--Kw5NH"
+  );
+  expect(await successMessage.textContent()).toContain("Моё обучение");
+});
